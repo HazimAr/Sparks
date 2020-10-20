3b45bd0886181ecd8a8711f2af5a18c9ba3aac9c
Nik Schaefer <nikkschaefer@gmail.com>
2020-10-19 23:44:27 -0500

    Added Contact form

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,23 +1,55 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
+firebase.initializeApp({
+    apiKey: "AIzaSyA4qr1j20J8yjzQO6UhwxNTPDQXR-KZBPk",
+    authDomain: "sparks-413c9.firebaseapp.com",
+    databaseURL: "https://sparks-413c9.firebaseio.com",
+    projectId: "sparks-413c9",
+    storageBucket: "sparks-413c9.appspot.com",
+    messagingSenderId: "1070946902875",
+    appId: "1:1070946902875:web:f9f5cd4c9621080cb7b07e",
+    measurementId: "G-PRXVXH5T20"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 const Contact = () => {
+
+    const messagesRef = firestore.collection('contactMessages'); // this is the chatroom we set up on firebase
+    const [formValue, setFormValue] = useState('');
+    const [formValueEmail, setFormValueEmail] = useState('');
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        console.log(formValueEmail, formValue)
+        await messagesRef.add({
+            text: formValueEmail,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            message: formValue,
+        });
+        setFormValue('');
+        setFormValueEmail('');
+    }
+    
+    
     return (
         <div>
-            <form id="contact-form">
-                <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" aria-describedby="emailHelp" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="message">Message</label>
-                    <textarea className="form-control" rows="5"></textarea>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+            <form onSubmit={handleSubmit}>
+                <label for="fname">Email</label>
+                <input type="text" placeholder="Email" onChange={(e) => setFormValueEmail(e.target.value)} value={formValueEmail} />
+
+                <label for="lname">Message</label>
+                <textarea type="text" placeholder="Message" onChange={(e) => setFormValue(e.target.value)} value={formValue} style={{height:'100px'}} />
+
+                <input type="submit" value="Submit" />
             </form>
         </div>
     )
--- src/Pages/NavBar.js
+++ src/Pages/NavBar.js
@@ -2,14 +2,13 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
+
+    
     return (
         <div>
             <header>
-                
                 <div className='nav-link-div'>
                     <NavLink to='/' className='nav-name'>Sparks Volleyball Club</NavLink>
-
-
                     <NavLink to='/' className='nav-link'>Home</NavLink>
                     <NavLink to='/coaches' className='nav-link'>Coaches</NavLink>
                     <NavLink to='/teams' className='nav-link'>Teams</NavLink>
