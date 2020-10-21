522ddda1ec5dda69f88a851a8aafd171c9d1a62c
Hazim Arafa <hazimarafa80@gmail.com>
2020-10-20 20:48:49 -0700

    niko

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import facebook from '../Images/facebook.png'
-import insta from '../Images/insta.png'
-import youtube from '../Images/youtube.png'
+import facebook from '../Images/facebook.svg'
+import insta from '../Images/instagram.svg'
+import youtube from '../Images/youtube.svg'
 
 const Footer = () => {
     return (
@@ -16,18 +16,14 @@ const Footer = () => {
                         <p>Las Vegas, Nevada</p>
                         <p>Sparks... The Revolution</p>
                     </div>
-                    {/* <div className='footer-content'>
-                    <p className='footer-header'>LOREM</p>
-                        <p>ipsum</p>
-                        <p>ipsum</p>
-                        <p>ipsum</p>
-                    </div> */}
+                    
                 </div>
                 <div className='footer-socials-div'>
                    <a href='https://www.facebook.com/SparksVBCLV'><img src={facebook} alt='' className='footer-socials' /></a>
                    <a href='https://www.instagram.com/sparks_vbc_lv/'><img src={insta} alt='' className='footer-socials' /></a>
                    <a href='https://www.youtube.com/channel/UCrUowRgcYRZif4HXd9RgO4A'><img src={youtube} alt='' className='footer-socials' /></a>
 
+                    
                 </div>
                 <p className='footer-copyright'>© 2020  Sparks VBC LV</p>
 
