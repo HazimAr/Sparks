709e5b52e27db099eb4cd767de218278c26574db
Hazim Arafa <hazimarafa80@gmail.com>
2020-10-28 16:44:26 -0700

    DONT USE WIX

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,7 +4,16 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-
+            <div className='home-section' id='home-whoAreWe'>
+                    <p className='home-title'>Who Are We?</p>
+                    <hr />
+                    <p className='home-paragraph'>
+                        Champion athletes aren’t just born - they’re selected, trained, molded and tested. We hone raw talent to maturity with quality coaching, primal workouts and a true love of the sport. Teamwork, perseverance, sportsmanship - those are the hallmarks of Sparks VBC LV / SVBC LV.
+                        <br /><br />
+                        Sparks Volleyball Club is committed to providing an excellent volleyball experience that promotes hard work, great attitudes, a genuine respect for the game, a “TEAM” concept at all times, and leadership development for each athlete. SVBC will provide the maximum opportunity for each athlete to train and compete at their highest level.
+                        <br /><br />
+                        No more hoping for the “good coach” or following coaches from club to club.</p>
+            </div>
         </div>
     )
 }
