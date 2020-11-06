73e20628913e66088941eefb47f5525ca820f269
Nik Schaefer <nikkschaefer@gmail.com>
2020-11-06 16:32:15 -0600

    Moved To Correct Place

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import Transparent from '../Images/Logos/Transparent-White.gif'
-import photo from '../Images/main/8-22-2_Tourney-86.jpg'
+import photo from '../Images/TourneyImage.jpg'
 
 const Top = (props) => {
     return (
