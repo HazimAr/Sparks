b2e2904201b080a549f7c3cdd2f773039ee5ee42
Hazim Arafa <hazimarafa80@gmail.com>
2020-11-07 18:24:40 -0800

    switched the pictures around

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -8,7 +8,7 @@ function RegisterComp() {
     return (
         <div className='about-register-div'>
             <div className='about-register-card'>
-                <img src={photo2} className='about-register-img' alt='' />
+                <img src={photo1} className='about-register-img' alt='' />
                 <div>
                     <p className='about-register-title'>Register</p>
                     <p className='about-register-text'>Champion athletes aren’t just born - they’re selected, trained, molded and tested. We hone raw talent to maturity with quality coaching, primal workouts and a true love of the sport. Teamwork, perseverance, sportsmanship - those are the hallmarks of Sparks VBC LV / SVBC LV.
@@ -33,7 +33,7 @@ const AboutUs = () => {
                     <p className='about-p'>Champion athletes aren’t just born - they’re selected, trained, molded and tested. We hone raw talent to maturity with quality coaching, primal workouts and a true love of the sport. Teamwork, perseverance, sportsmanship - those are the hallmarks of Sparks VBC LV / SVBC LV.</p>
                     <p className='about-p'>Sparks Volleyball Club is committed to providing an excellent volleyball experience that promotes hard work, great attitudes, a genuine respect for the game, a “TEAM” concept at all times, and leadership development for each athlete. SVBC will provide the maximum opportunity for each athlete to train and compete at their highest level. No more hoping for the “good coach” or following coaches from club to club.</p>
                 </div>
-                <img alt='' src={photo1} className='about-vert' />
+                <img alt='' src={photo2} className='about-vert' />
             </div>
             <RegisterComp />
         </div>
