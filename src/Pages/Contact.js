8cd22fd4da5fd46345909e7bb6f7a607100b5c96
Nik Schaefer <nikkschaefer@gmail.com>
2020-11-07 23:00:04 -0600

    Subject Contact Us Formatting

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -58,19 +58,21 @@ const Contact = () => {
             <p>Las Vegas, Nevada</p>
 
             <form className='contact-form' onSubmit={handleSubmit}>
-                <p className='label'>Name *</p>
-                <input type="text" placeholder="Name" onChange={(e) => setUserName(e.target.value)} value={userName} />
+                <div className='contact-column'>
+                    <p className='label'>Name *</p>
+                    <input type="text" placeholder="Name" onChange={(e) => setUserName(e.target.value)} value={userName} />
 
-                <p className='label'>Email *</p>
-                <input type="text" placeholder="Email" onChange={(e) => setUserEmail(e.target.value)} value={userEmail} />
+                    <p className='label'>Email *</p>
+                    <input type="text" placeholder="Email" onChange={(e) => setUserEmail(e.target.value)} value={userEmail} />
+                </div>
+                <div className='contact-column'>
+                    <p className='labelRight'>Phone Number *</p>
+                    <input type="text" id='rightColInput' placeholder="111-111-1111" onChange={(e) => setUserPhone(e.target.value)} value={userPhone} />
 
-                <p className='label'>Phone Number *</p>
-                <input type="text" placeholder="111-111-1111" onChange={(e) => setUserPhone(e.target.value)} value={userPhone} />
-
-                <p className='label'>Subject *</p>
-                <input type="text" placeholder="Subject" onChange={(e) => setUserSubject(e.target.value)} value={userSubject} />
-
-                <p className='label'>Message *</p>
+                    <p className='labelRight'>Subject *</p>
+                    <input type="text" id='rightColInput' placeholder="Subject" onChange={(e) => setUserSubject(e.target.value)} value={userSubject} />
+                </div>
+                <p className='label' id='messageLabel'>Message *</p>
                 <textarea type="text" placeholder="Message" onChange={(e) => setUserMessage(e.target.value)} value={userMessage} style={{ height: '200px' }} />
                 <input type="submit" value="SUBMIT" />
                 <p>{errorMessage}<br /></p>
