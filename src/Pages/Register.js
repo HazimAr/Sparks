83886205b56c24e96c1e4013b0554098b5690dd4
Nik Schaefer <nikkschaefer@gmail.com>
2020-11-07 23:06:53 -0600

    Iframe Working

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -6,7 +6,7 @@ const Register = () => {
     return (
         <div>
             <div className='register-form'>
-                <iframe title='haxor' src="https://docs.google.com/forms/d/e/1FAIpQLSea_RocZ91i80gbLWzngi25YOXzX-m3MhWRlIDVIX2F0YEqww/viewform?embedded=true" width="640" height="650" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                <iframe title='haxor' style={{width:'100%'}} src="https://docs.google.com/forms/d/e/1FAIpQLSea_RocZ91i80gbLWzngi25YOXzX-m3MhWRlIDVIX2F0YEqww/viewform?embedded=true" width="640" height="650" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
             </div>
         </div>
     )
